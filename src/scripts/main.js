e36dfaec8497d1bdf0d929c40014acf197fc3d72
Nicolay <nicolay.trygub@popel-studio.com>
2015-03-19 22:05:58 +0200

    selectize added

---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    $('.selectize').selectize({
+        sortField: 'text'
+    });
+
+    $('.selectize-tag').selectize({
+        plugins: ['remove_button'],
+        delimiter: ',',
+        persist: false,
+        create: function(input) {
+            return {
+                value: input,
+                text: input
+            }
+        }
+    });
+});
