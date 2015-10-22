e8e702606856999af295622f771d9801a8bb324f
alexander <aleksandr.dankov@popel.com.ua>
2015-10-22 19:06:39 +0300

    up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
+//in viewport
+(function(e){e.fn.visible=function(t,n,r){var i=e(this).eq(0),s=i.get(0),o=e(window),u=o.scrollTop(),a=u+o.height(),f=o.scrollLeft(),l=f+o.width(),c=i.offset().top,h=c+i.height(),p=i.offset().left,d=p+i.width(),v=t===true?h:c,m=t===true?c:h,g=t===true?d:p,y=t===true?p:d,b=n===true?s.offsetWidth*s.offsetHeight:true,r=r?r:"both";if(r==="both")return!!b&&m<=a&&v>=u&&y<=l&&g>=f;else if(r==="vertical")return!!b&&m<=a&&v>=u;else if(r==="horizontal")return!!b&&y<=l&&g>=f}})(jQuery)
+
+//
+    $(window).on("scroll", function(){
+        if($('.accept').visible(true)){
+            $('.icon').addClass('slide-in');
+        }
+    });
+
+
 (function ($) {
     "use strict";
 
@@ -29,6 +40,11 @@
     $('.accept').on("click",function(){
         $('.icon').addClass('slide-in');
     });
-
+    $(window).on('load', function () {
+        var $preloader = $('.page-preloader'),
+            $spinner   = $preloader.find('.preloader-itself');
+        $spinner.fadeOut();
+        $preloader.delay(350).fadeOut('slow');
+    });
 
 })(window.jQuery);
